a7f7878240f60a8ed97385b40854bd4ee2d24d68
percyfikri <mafikri27@gmail.com>
2023-03-31 09:31:14 +0700

    insert user (admin)

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 
 class UserSeeder extends Seeder
 {
@@ -12,6 +14,10 @@ class UserSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+     DB::table('users')->insert([
+        'name' => 'Administrator',
+        'email' => 'admin@admin',
+        'password' => Hash::make('password'),
+     ]);  
     }
 }
